068ac49e2d428f330def3b1331fa22357b38146e
MattyJ007 <matt.jdickie007@gmail.com>
2024-05-04 12:07:14 +1000

    Update for new model to do list
    COMP-5329

--- dataloading.py
+++ dataloading.py
@@ -13,16 +13,17 @@ print(df.head(10))
 Done
 1. Access subset of training data and loop through each row
 2. Find largest images portrait and landscape - resize all images to same.
-3. Greyscale than normalise (pixel values between 0 - 1) (Reshape to resnet's expected shapeg)
+3. Greyscale than normalise (pixel values between 0 - 1)
 4. One hot encode labels + check label distribution within training set
 5. Preprocess captions - remove stop words + lowercasing + tokenise
 
 To Do
 6. Visualise the data at the different stages
-7. Build model with PyTorch off of ResNet18? (maybe check out other multilabel models) (motivation for resenet18 - some of our classes have very few examples - training from scratch would give very poor performance)
+7. Explore new models proposed in tute
+7. Build model with PyTorch - see if papers have pretrained models available (DEit, ViT, etc.) (maybe check out other multilabel models) (motivation for pretrained model - some of our classes have very few examples - training from scratch would give very poor performance)
    - Use untrained architecture
    - use pretrained architecture
-   - (optional) Map ResNet18 classes to our classes and check accuracy - play around with whether resnet18 can do multi label
+   - (optional) Map pretrained model classes to our classes and check accuracy - play around with whether pretrained model can do multi label
    - pretrained language model
 8. Save model to file
 9. Add F1 score
