f379ce1c85c3a38ee9409b336c1b3bb8eacae54d
MattyJ007 <matt.jdickie007@gmail.com>
2024-05-04 12:00:10 +1000

    Add work list
    COMP-5329

--- dataloading.py
+++ dataloading.py
@@ -10,22 +10,31 @@ with open(FILENAME) as file:
 print(df.head(10))
 
 '''
+Done
 1. Access subset of training data and loop through each row
 2. Find largest images portrait and landscape - resize all images to same.
-3. Greyscale than normalise (pixel values between 0 - 1)
+3. Greyscale than normalise (pixel values between 0 - 1) (Reshape to resnet's expected shapeg)
 4. One hot encode labels + check label distribution within training set
 5. Preprocess captions - remove stop words + lowercasing + tokenise
+
+To Do
 6. Visualise the data at the different stages
-7. Build model with PyTorch
-8. Add F1 score
-9. Add improvements
+7. Build model with PyTorch off of ResNet18? (maybe check out other multilabel models) (motivation for resenet18 - some of our classes have very few examples - training from scratch would give very poor performance)
+   - Use untrained architecture
+   - use pretrained architecture
+   - (optional) Map ResNet18 classes to our classes and check accuracy - play around with whether resnet18 can do multi label
+   - pretrained language model
+8. Save model to file
+9. Add F1 score
+10. Hyperparameter tuning
+11. Ablation tests based on above baselines
+- baseline with and without pretraining
+- remove captions
+- remove images
+12. Add improvements
  - Piggyback off of pretrained models
  - cnn layers
  - preprocessing
  - Low rank adaption + parameter efficient fine tuning
  - Split image to increase training sets
-'''
-
-'''
-Freeze 70% of the resnet18 first layers. Reduce output to our label output size. Retrain the output layers.
 '''
